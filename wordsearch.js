c7484c6caf9ee9ff3f2053f884254794165fe8e8
Anto Ter-Mesropian <antotm99@gmail.com>
2021-01-13 21:33:11 +0000

    first commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalWord = [];
+    const verticalWord = [];
+    const newArray = [];
+    
+    for (let word of horizontalJoin) {
+        horizontalWord.push(word)
+    }``
+    for(let i = 0; i < letters[0].length; i++){
+        let output = [];
+          for(let j = 0; j < letters.length; j++){
+            output.push(letters[j][i]);
+          };
+          newArray.push(output);
     }
+    const newStringArray = newArray.map(a => a.join(''))      
+
+    for (let word of newStringArray) {
+        verticalWord.push(word)
+    }  
+
+    if (horizontalWord.includes(word) || verticalWord.includes(word)){
+        return true;
+    } else { return false}
+
 }
 
 module.exports = wordSearch
